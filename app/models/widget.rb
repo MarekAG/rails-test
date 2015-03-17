5137af1ca142b11420873283d726f58fba877282
MarekAG <marekwstrzala@gmail.com>
2015-03-17 10:43:24 +0100

    fix

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -1,6 +1,5 @@
 class Widget < ActiveRecord::Base
   validates :name, presence: true
   validates :description, length: { minimum: 2 }
-  validates :xd, presence: true
   validates :stock, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
